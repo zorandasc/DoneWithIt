f1896fd223e8649fca57d10eb3707c24aa483ab8
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-04-17 22:09:46 +0200

    sundey end

--- App.js
+++ App.js
@@ -1,29 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  console.log("ckapi");
-
-  const handlePress = () => {
-    console.log("dick pressed");
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text  onPress={handlePress}>
-        Open up App.js to start working on your app! 
-      </Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <ViewImageScreen></ViewImageScreen>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  
-});
+const styles = StyleSheet.create({});
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,52 @@
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+import colors from "../config/colors";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/logo-red.png")}
+        ></Image>
+        <Text>Sell What We Dont Need</Text>
+      </View>
+
+      <View style={styles.logginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.secondary,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 100,
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default WelcomeScreen;
